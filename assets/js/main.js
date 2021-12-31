7517a86550255b57e17be4635b6e2bbb7adc549c
Felipe <felipee.lf16@gmail.com>
2021-12-31 05:46:59 -0300

    se agrega jquery, modal, marcadores, otros

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,13 @@
+
+// tooltips
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]')
   );
   var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl);
   });
+
+//   scroll
+var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+    target: '#navbar-example'
+  })
+
